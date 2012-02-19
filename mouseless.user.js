7f36666905231ac46959aa747fbb3252023bc178
BVK Chaitanya <bvkchaitanya@gmail.com>
2012-02-19 00:13:47 -0800

    Initial version.

---
+++ mouseless.user.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name           mouseless
+// @namespace      http://www.userscripts.org
+// @description    Internet browsing without mouse.
+// @include        *
+// ==/UserScript==
+
+// Some global variables. FIXME: Global variables are bad.
+var keyZero = 48
+var keyNine = 57
+var keyReturn = 13
+var keysCaptured = ""
+
+// Perform a click on an link.
+function clickAnchor(anchor) {
+    try {
+        anchor.click()
+    } catch (ex) {
+        // If click() method doesn't work!
+        var event = document.createEvent("MouseEvents")
+        event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                             false, false, false, false, 0, null);
+        anchor.dispatchEvent(event);
+    }
+}
+
+// Capture keyboard events and take action if necessary.
+function captureKey(event) {
+    if (event.keyCode == keyReturn && keysCaptured.length != 0) {
+        var anchors = document.body.getElementsByTagName("a")
+        for (var i = 0; i < anchors.length; i++) {
+            var anchor = anchors[i]
+            var linkIndex = anchor.getAttribute('linkIndex')
+            if (!linkIndex)
+                continue
+
+            if (linkIndex == parseInt(keysCaptured))
+                clickAnchor(anchor)
+        }
+        keysCaptured = ""
+    } else if (event.keyCode >= keyZero && event.keyCode < keyNine) {
+        keysCaptured += event.keyCode - keyZero
+    } else {
+        keysCaptured = ""
+    }
+}
+
+// Apply a counter to each link.
+function resetLinks() {
+    var anchors = document.body.getElementsByTagName("a")
+    for (var i = 0; i < anchors.length; i++)
+        anchors[i].setAttribute('linkIndex', i)
+}
+
+// Update the CSS stylesheet to display link counters.
+function updateStyleSheet() {
+    var head = document.getElementsByTagName('head')[0]
+    head.appendChild(document.createElement('style'))
+    var style = document.styleSheets[document.styleSheets.length - 1]
+    style.insertRule("a:after { " +
+                     "  content: attr(linkIndex);" +
+                     "  font-size: .5em;" +
+                     "  font-style: italic;" +
+                     "  margin: 1px;" +
+                     "  vertical-align: super;" +
+                     "}", 0)
+}
+
+resetLinks()
+updateStyleSheet()
+window.addEventListener("keydown", captureKey, true)
